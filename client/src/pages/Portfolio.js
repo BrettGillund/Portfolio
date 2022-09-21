d130ffa4ef9ae50c8003839daef1f9f37cf8d0c6
Brett Gillund <brettgillund2@gmail.com>
2022-09-21 18:13:09 -0500

    adding links to contact page

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -14,8 +14,8 @@ function Portfolio() {
                 <div class="portfolio-column">
                     <h1 class="portfolio-name">GamR</h1>
                     <div >
-                        <a href="https://gamr-applicaiton.herokuapp.com/" target="_blank" class="portfolio-links">- Deployed Application: Gamr</a>
-                        <a href="https://github.com/BrettGillund/Gamr" target="_blank" class="portfolio-links">- Github: Gamr</a>
+                        <a href="https://gamr-applicaiton.herokuapp.com/" target="_blank" rel="noreferrer" class="portfolio-links">- Deployed Application: Gamr</a>
+                        <a href="https://github.com/BrettGillund/Gamr" target="_blank" rel="noreferrer" class="portfolio-links">- Github: Gamr</a>
                     </div>
                 </div>
                 <p class="portfolio-description"> GamR is an application made for gamers, by gamers. Upon launching the application the user is presented with a landing page that walks the user through how to use the application and all the features. A new user is able to register or login in the top right corner. Once the user is logged in they are able to navigate to the sidebar on the left which includes a Profile page and a Users page. Profile allows a user to view and edit their information, while visiting the Users page allows the logged in user to browse other GamR's profile's and connect with people with similar interests!</p>
@@ -26,8 +26,8 @@ function Portfolio() {
                 <div class="portfolio-column">
                     <h1 class="portfolio-name">Off The Shelf</h1>
                     <div >
-                        <a href="https://off-the-shelf-project-2.herokuapp.com/" target="_blank" class="portfolio-links">Deployed Application: Off The Shelf</a>
-                        <a href="https://github.com/jbuck123/Project-2" target="_blank" class="portfolio-links">Github: Off The Shelf</a>
+                        <a href="https://off-the-shelf-project-2.herokuapp.com/" target="_blank" rel="noreferrer" class="portfolio-links">Deployed Application: Off The Shelf</a>
+                        <a href="https://github.com/jbuck123/Project-2" target="_blank" rel="noreferrer" class="portfolio-links">Github: Off The Shelf</a>
                     </div>
                 </div>
                 <p class="portfolio-description">Off the shelf is an application that allows a user to create a profile and save their library collection to their profile. There they can manage their book collection, and utilize their list to pick which book they would like to read next.</p>
@@ -38,8 +38,8 @@ function Portfolio() {
                 <div class="portfolio-column">
                     <h1 class="portfolio-name">Drink Finder</h1>
                     <div >
-                        <a href="https://b-alt-del.github.io/Project-One/" target="_blank" class="portfolio-links">Deployed Application: Drink Finder</a>
-                        <a href="https://github.com/B-alt-del/Project-One" target="_blank" class="portfolio-links">Github: Drink Finder</a>
+                        <a href="https://b-alt-del.github.io/Project-One/" target="_blank" rel="noreferrer" class="portfolio-links">Deployed Application: Drink Finder</a>
+                        <a href="https://github.com/B-alt-del/Project-One" target="_blank" rel="noreferrer" class="portfolio-links">Github: Drink Finder</a>
                     </div>
                 </div>
                 <p class="portfolio-description"> Drink Finder is an application that allows the user to create cocktails or mixed drinks based on the ingredients they select. The user will be able to look at whatever alcohol they have on hand or plan on purchasing, select that alcohol in the app, and generate a list of recipes for cocktails based on their selection. The app will also feature a map function where the user can search for liquor stores to find the nearest stores near them to purchase any ingredients they need.</p>
